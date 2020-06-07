82e2d646d4cf9837ad5e0edc69cfbaca4e5ed123
Rahul Dewan <44262872+rahul3333@users.noreply.github.com>
2020-06-08 04:37:22 +0530

    Remove Client ID and Secret

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -4,8 +4,8 @@ const crypto=require('crypto');
 const User=require('../models/user');
 
 passport.use(new googleStrategy({
-    clientID:"",//Enter your client ID
-    clientSecret:"",//Enter your Client Secret
+    clientID:"",//Here Enter your client ID
+    clientSecret:"",//Here Enter your Client Secret
     callbackURL:"http://localhost:8000/users/auth/google/callback"
 },
 
